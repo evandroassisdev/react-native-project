9ced5afcdac2b577027bfd3506864dd137cd5016
Evandro Assis <evandroassisdev@gmail.com>
2019-09-16 16:02:49 -0400

    Finalizando Desafio

--- Projeto/src/index.js
+++ Projeto/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 }
